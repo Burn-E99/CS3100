7b37e3b9dd61d3fc7ce18f85598fcc8784b36cc8
Ean Milligan <ean.milligan@gmail.com>
2017-09-28 00:29:15 -0400

    .

---
+++ Project1/Qqueue.cpp
@@ -0,0 +1,108 @@
+#include "Qqueue.h"
+#include <iostream>
+
+// Constructor
+Qqueue::Qqueue(int c) : Capacity(c) {
+	DynamicQueue = new std::string[Capacity];
+	
+	front = 0;
+	back = 0;
+	num = 0;
+};
+//Copier
+Qqueue::Qqueue(const Qqueue& q) : Capacity(q.size()) {
+	DynamicQueue = new std::string[Capacity];
+	front = 0;
+	back = q.size();
+	num = q.size();
+
+	for (int i=0;i<num;i++) {
+		try {
+			DynamicQueue[i] = *(&q.getfront() + i);
+		}
+		catch(...) {
+			break;
+		}
+	}
+	for (int i=0;i<num;i++) {
+		try {
+			DynamicQueue[num - 1 - i] = *(&q.getback() - i);
+		}
+		catch(...) {
+			break;
+		}
+	}
+};
+
+Qqueue::~Qqueue() {
+	delete DynamicQueue;
+};
+
+void Qqueue::enqueue(const std::string& s) {
+	if (num == Capacity) {
+		throw;
+	}
+
+	DynamicQueue[back%Capacity] = s;
+	back++;
+	num++;
+};
+
+std::string Qqueue::dequeue() {
+	if (num == 0) {
+		throw;
+	}
+	std::string temp = DynamicQueue[front];
+	DynamicQueue[front] = "";
+	front++;
+	num--;
+
+	if (front == Capacity) {
+		front = 0;
+	}
+
+	return temp;
+};
+
+std::string& Qqueue::getfront() const {
+	if(num == 0) {
+		throw;
+	}
+	return DynamicQueue[front];
+};
+
+std::string& Qqueue::getback() const {
+	if(num == 0) {
+		throw;
+	}
+	return DynamicQueue[(back-1)%Capacity];
+};
+
+bool Qqueue::IsEmpty() const {
+	return num == 0;
+};
+
+void Qqueue::print() const {
+
+	for (int i=0;i<num;i++) {
+		std::cout<<DynamicQueue[(front+i)%Capacity];
+
+		if (i != num - 1) {
+			std::cout << ", ";
+		}
+	}
+};
+
+int Qqueue::size() const {
+	return num;
+};
+
+int Qqueue::getCapacity() const {
+	return Capacity;
+};
+
+bool Qqueue::equals(const Qqueue& q) const {
+	
+	
+	return true;
+};
---
+++ Project1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include "Qqueue.h"
+
+int main(){
+	Qqueue test = Qqueue(4);
+	std::cout<<test.IsEmpty();
+	test.print();
+	test.enqueue("1");
+	test.enqueue("2");
+	test.enqueue("3");
+	test.enqueue("4");
+	test.print();
+	test.dequeue();
+	std::cout<<std::endl;
+	test.enqueue("5");
+	test.dequeue();
+	test.dequeue();
+	test.dequeue();
+	test.enqueue("6");
+	test.enqueue("7");
+	test.enqueue("8");
+
+	std::cout<<test.getfront();
+	std::cout<<test.getback();
+	std::cout<<test.IsEmpty();
+	std::cout<<test.size();
+	
+	test.print();
+
+	std::cout<<std::endl;
+
+	Qqueue test2 = Qqueue(test);
+
+	test2.print();
+}
