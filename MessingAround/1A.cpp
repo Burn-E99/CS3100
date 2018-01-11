c31e04955f64f0a8febe3520720b440c02efa270
Ean Milligan <ean.milligan@gmail.com>
2018-01-11 11:22:45 -0500

    Added a messing around area

---
+++ MessingAround/1A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// Construction
+	int x(10);
+	double y = 25.0;
+	char c{ 'A' };
+	bool b(true);
+
+	cout << "enter x, y, c, b: ";
+
+	cin >> x >> y >> c >> b;
+
+	cout << x << y << c << b;
+
+	return 0;
+}
